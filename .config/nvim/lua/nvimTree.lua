63ef00577bbbe7d42561ebd1d125a40779881a75
hown3d <ludi.origin@gmail.com>
2021-07-13 02:17:58 +0200

    nvimtree

--- .config/nvim/lua/nvimTree.lua
+++ .config/nvim/lua/nvimTree.lua
@@ -50,8 +50,8 @@ M.config = function()
     local tree_cb = require "nvim-tree.config".nvim_tree_callback
 
     g.nvim_tree_bindings = {
-        {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
-        {key = {"<2-RightMouse>", "<C-}>"}, cb = tree_cb("cd")},
+        {key = {"<CR>", "o", "<C-LeftMouse>"}, cb = tree_cb("edit")},
+        {key = {"<C-RightMouse>", "<C-}>"}, cb = tree_cb("cd")},
         {key = "<C-v>", cb = tree_cb("vsplit")},
         {key = "<C-x>", cb = tree_cb("split")},
         {key = "<C-t>", cb = tree_cb("tabnew")},
@@ -76,8 +76,8 @@ M.config = function()
         {key = "y", cb = tree_cb("copy_name")},
         {key = "Y", cb = tree_cb("copy_path")},
         {key = "gy", cb = tree_cb("copy_absolute_path")},
-        {key = "[c", cb = tree_cb("prev_git_item")},
-        {key = "}c", cb = tree_cb("next_git_item")},
+        {key = "#c", cb = tree_cb("prev_git_item")},
+        {key = "+c", cb = tree_cb("next_git_item")},
         {key = "-", cb = tree_cb("dir_up")},
         {key = "q", cb = tree_cb("close")},
         {key = "g?", cb = tree_cb("toggle_help")}
